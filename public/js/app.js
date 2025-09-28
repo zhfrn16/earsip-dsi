72609308439c0f74613c7c23cff280b5177f891a
Rafi Maryudwika <northernosprey2013@gmail.com>
2025-09-28 18:05:37 +0000

    Updated app.js

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Jun\Documents\BeCuk\siArsip\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Jun\Documents\BeCuk\siArsip\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! /workspaces/earsip-dsi/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /workspaces/earsip-dsi/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
