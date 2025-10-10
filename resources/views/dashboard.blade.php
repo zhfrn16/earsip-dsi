9b3bdfdbf6e1355eb53729d0f2fcf7f2f478835e
Rafi Maryudwika <northernosprey2013@gmail.com>
2025-10-10 13:20:00 +0000

    Removed users in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,21 +6,6 @@
 <div class="row">
     <div class="col-lg-3 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
-        <div class="card-icon bg-primary">
-            <i class="far fa-user"></i>
-        </div>
-        <div class="card-wrap">
-            <div class="card-header">
-            <h4>Total User</h4>
-            </div>
-            <div class="card-body">
-            {{$totalUser}}
-            </div>
-        </div>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-        <div class="card card-statistic-1">
         <div class="card-icon bg-danger">
             <i class="far fa-newspaper"></i>
         </div>
@@ -64,8 +49,6 @@
         </div>
         </div>
     </div>
-</div>
-<div class="row">
     <div class="col-lg-3 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
         <div class="card-icon bg-info">
