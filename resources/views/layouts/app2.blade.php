00808b57a0d3c805f8d43e89575953cfcf1d1578
Rafi Maryudwika <northernosprey2013@gmail.com>
2025-10-10 15:44:22 +0000

    Optimized performance

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -5,12 +5,35 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('title')</title>
-
+    <link rel="preload" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.22/b-1.6.5/b-flash-1.6.5/b-html5-1.6.5/b-print-1.6.5/cr-1.5.2/fc-3.3.1/fh-3.1.7/kt-2.5.3/r-2.2.6/rg-1.1.2/rr-1.2.7/sc-2.0.3/sb-1.0.0/sp-1.2.1/sl-1.3.1/datatables.min.css" as="style">
+    <link rel="preload" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" as="style" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin>
+    <link rel="preload" href="{{ url('css/fontawesome/all.css') }}" as="style" crossorigin>
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/select2@4.0.6/dist/css/select2.min.css" as="style">
+    <link rel="preload" href="{{ url('stisla/assets/css/style.css')}}" as="style">
+    <link rel="preload" href="{{ url('stisla/assets/css/components.css')}}" as="style">
+    <link rel="preload" href="https://code.jquery.com/jquery-3.3.1.min.js" as="script" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin>
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/select2@4.0.6/dist/js/select2.min.js" as="script">
+    <link rel="preload" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css" as="style">
+    <link rel="preload" href="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js" as="script"integrity="sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E=" crossorigin>
+    <link rel="preload" href="https://code.jquery.com/jquery-migrate-3.0.0.min.js" as="script" >
+    <link rel="preload" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.22/b-1.6.5/b-flash-1.6.5/b-html5-1.6.5/b-print-1.6.5/cr-1.5.2/fc-3.3.1/fh-3.1.7/kt-2.5.3/r-2.2.6/rg-1.1.2/rr-1.2.7/sc-2.0.3/sb-1.0.0/sp-1.2.1/sl-1.3.1/datatables.min.js" as="script" >
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/pdfmake@0.1.36/build/pdfmake.min.js" as="script" >
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/pdfmake@0.1.36/build/vfs_fonts.js" as="script" >
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" as="script" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin>
+    <link rel="preload" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" as="script" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin>
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/jquery.nicescroll@3.7.6/jquery.nicescroll.min.js" as="script" >
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/moment@2.24.0/min/moment.min.js" as="script" >
+    <link rel="preload" href="https://unpkg.com/sweetalert/dist/sweetalert.min.js" as="script" >
+    <link rel="preload" href="//cdn.jsdelivr.net/npm/sweetalert2@11" as="script" >
+    <link rel="preload" href="{{ url('stisla/assets/js/stisla.js') }}" as="script">
+    <link rel="preload" href="{{ url('stisla/assets/js/scripts.js') }}" as="script">
+    <link rel="preload" href="{{ url('js/app2.js') }}" as="script">
+    <link rel="preload" href="{{ url('stisla/assets/js/page/index.js') }}" as="script">
   <!-- General CSS Files -->
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.22/b-1.6.5/b-flash-1.6.5/b-html5-1.6.5/b-print-1.6.5/cr-1.5.2/fc-3.3.1/fh-3.1.7/kt-2.5.3/r-2.2.6/rg-1.1.2/rr-1.2.7/sc-2.0.3/sb-1.0.0/sp-1.2.1/sl-1.3.1/datatables.min.css"/>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="{{ url('css/fontawesome/all.css') }}" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/select2@4.0.6/dist/css/select2.min.css" rel="stylesheet" />
   {{-- <!-- CSS Libraries -->
   <link rel="stylesheet" href="../node_modules/jqvmap/dist/jqvmap.min.css">
   <link rel="stylesheet" href="../node_modules/summernote/dist/summernote-bs4.css">
@@ -21,7 +44,7 @@
   <link rel="stylesheet" href="{{ url('stisla/assets/css/style.css')}}">
   <link rel="stylesheet" href="{{ url('stisla/assets/css/components.css')}}">
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/select2@4.0.6/dist/js/select2.min.js"></script>
   <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
   <style>
   ul.ui-autocomplete {
@@ -60,12 +83,12 @@
   <script src="https://code.jquery.com/jquery-migrate-3.0.0.min.js"></script>
   <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.22/b-1.6.5/b-flash-1.6.5/b-html5-1.6.5/b-print-1.6.5/cr-1.5.2/fc-3.3.1/fh-3.1.7/kt-2.5.3/r-2.2.6/rg-1.1.2/rr-1.2.7/sc-2.0.3/sb-1.0.0/sp-1.2.1/sl-1.3.1/datatables.min.js"></script>
   <script src="{{url('/js/dataTables.rowsGroup.js')}}"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/pdfmake@0.1.36/build/pdfmake.min.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/pdfmake@0.1.36/build/vfs_fonts.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/jquery.nicescroll@3.7.6/jquery.nicescroll.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/moment@2.24.0/min/moment.min.js"></script>
   <script src="{{url('/js/jquery.rowspanizer.min.js')}}"></script>
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
